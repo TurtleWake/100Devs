a3e7a69cd6325c39ef051a315474aa234c2ce1a8
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2024-10-16 17:38:12 -0400

    updated "Add conditions" main page, working on JS functions.

---
+++ The JS way/Coding Time!/html/Chapters/3-conditions/js/conditions.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+// Following day
+
+document.querySelector("#d1").innerHTML = 
+`
+<input type='text' id='userInput' placeholder='Day...'>
+<button id='usersDayChoice'><i class='bx bxs-calendar-check'></i></button>
+`
+document.querySelector("#usersDayChoice").addEventListener("click", nextDay);
+
+function nextDay(){
+    let userDay = document.querySelector("#userInput").value.toLowerCase();
+    console.log("click")
+    const weekDays = [
+        "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"
+    ];
+
+    if (userDay === weekDays){
+
+    }
+}
