93ddbe106bab81f7428e1e898b6e8942a6daf5aa
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2024-10-18 16:17:03 -0400

    Set up header and question for "Number comparison"

--- The JS way/Coding Time!/html/Chapters/3-conditions/js/conditions.js
+++ The JS way/Coding Time!/html/Chapters/3-conditions/js/conditions.js
@@ -1,9 +1,12 @@
-
-
-
-
-
 // Following day
+const h2 = document.querySelector('h2');
+const pre1 = document.querySelector('#pre1');
+const code = document.querySelector('#code1');
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+const d1 = document.querySelector('#d1');
+const p3 = document.querySelector('#p3');
+const p4 = document.querySelector('#p4');
 
 document.querySelector("#d1").innerHTML = 
 `
@@ -12,14 +15,48 @@ document.querySelector("#d1").innerHTML =
 `
 document.querySelector("#usersDayChoice").addEventListener("click", nextDay);
 
+function removePreviousInfo(){
+    h2.innerText = "";
+    p1.innerText = "";
+    p2.innerText = "";
+    p3.innerText = "";
+    p4.innerText = "";
+    d1.innerText = "";
+}
+
 function nextDay(){
     let userDay = document.querySelector("#userInput").value.toLowerCase();
-    console.log("click")
     const weekDays = [
         "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"
     ];
+    
+    const currentIndex = weekDays.indexOf(userDay);
 
-    if (userDay === weekDays){
-        return 
+    if(currentIndex === -1){
+        p3.innerHTML = "Invalid day entered. Please enter a valid day of the week.";
+    } else{
+        const nextDayIndex = (currentIndex + 1) % weekDays.length;
+
+        p3.innerHTML = "The next day of the week is " + 
+            weekDays[nextDayIndex].charAt(0).toUpperCase() + weekDays[nextDayIndex].slice(1);
+
+        p4.innerHTML = 
+        `
+        Let's move on to the next problem! <i class='bx bxs-right-arrow' id='nextArrow'></i>
+        `
+
+        document.querySelector('#nextArrow').addEventListener('click', nextPage); 
     }
+}
+
+function nextPage(){
+    removePreviousInfo();
+    h2.innerText = 'Number comparison'
+    p1.innerText = 'Write a program that accepts two numbers, then compares their values and displays an appropriate message in all cases.'
+    d1.innerHTML = 
+    `
+    <input id='number1'>
+    <p>Compared to</p>
+    <input id='number2'>
+    `
 }
