9371e07c92ce8cca0ac9e760e1d1d0eaf28a5fb2
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2024-11-01 17:47:45 -0400

    Set up the countdown, changes number in the button. Now adding an input for the user.

--- The JS way/Coding Time!/html/Chapters/4-repeatStatements/js/repeatStatements.js
+++ The JS way/Coding Time!/html/Chapters/4-repeatStatements/js/repeatStatements.js
@@ -1,68 +1,19 @@
-// three.js libray
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// allow the camera to move
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// allow importing the file
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-
 document.querySelector('#d1 button').addEventListener('click', countDown);
 
-function countDown(){
-    let count = parseInt(document.querySelector('#turnCount').innerText);  
-    while(count > 0){
-        count--;
-        console.log("count = ", count);
-    }
-    
+function countDown() {
+    let count = parseInt(document.querySelector('#turnCount').innerText);
+
+    const intervalId = setInterval(() => {
+        if (count > 0) {
+            count--;
+            document.querySelector('#turnCount').innerText = count;
+        } else if(count === 0){
+            clearInterval(intervalId);
+            count.innerHTML = `<input id='userTurnCount' placeholder='#'>`
+        } else {
+            clearInterval(intervalId); 
+        }
+    }, 1000); 
 }
 
-// three.js scene
-const scene = new THREE.Scene();
-// new camera and angles
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-let object;
-let controls;
-let objToRender = 'eye';
 
-const loader = new GLTFLoader();
-
-loader.load(
-    `models/${objToRender}/scene.gltf`,
-    function (gltf) {
-        object = gltf.scene;
-        scene.add(object);
-    },
-    function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    function (error) {
-        console.error(error);
-    }
-);
-
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("container3D").appendChild(renderer.domElement);
-camera.position.z = objToRender === "dino" ? 25 : 500;
-
-// lights
-
-const topLight = new THREE.DirectionalLight(0xffffff, 1);
-topLight.position.set(500, 500, 500)
-topLight.castShadow = true;
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-scene.add(ambientLight);
-
-function animate() {
-    requestAnimationFrame(animate);
-}
-
-window.addEventListener("resize", function () {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-animate();
