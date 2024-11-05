aa476feb9522db9762fd9f18db81d0df0d50d0dd
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2024-11-05 17:52:36 -0500

    Set up new count down system.

---
+++ The JS way/Coding Time!/html/Chapters/4-repeatStatements/js/repeatStatements.js
@@ -0,0 +1,16 @@
+document.querySelector('#d1 button').addEventListener('click', countDown);
+let count = document.querySelector('#turnCount').innerText;
+
+console.log('Count: ', count)
+
+function countDown(){
+    setInterval(() => {
+        if(count > 0){
+            count--;
+            document.querySelector('#turnCount').innerText = count;
+        } else if (count === 0){
+            document.querySelector('#turnCount').innerHTML = `<input id='userCount'>`
+        }
+    }, 1000)
+}
+
