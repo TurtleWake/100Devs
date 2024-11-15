f644b033940e99edb11c56e944ce2a57fa757919
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2024-11-15 15:52:35 -0500

    Finished Carousel, Parity, and Input Validation. Working on little game for guessing numbers.

--- The JS way/Coding Time!/html/Chapters/4-repeatStatements/js/repeatStatements.js
+++ The JS way/Coding Time!/html/Chapters/4-repeatStatements/js/repeatStatements.js
@@ -17,6 +17,7 @@ function removePreviousInfo(){
     document.querySelector("#p1").innerText = "";
     document.querySelector("#p2").innerText = "";
     document.querySelector("#p3").innerText = "";
+    document.querySelector("#p4").innerText = "";
     document.querySelector("#d1").innerText = "";
     document.querySelector("#d2").innerText = "";
 }
@@ -65,32 +66,64 @@ function showInputField() {
 
 // ****Parity**** //
 
-function parityPage(){
+function parityPage() {
     removePreviousInfo();
-    h2.innerText = 'Parity'
-    p1.innerText = 'Check the following program that shows even numbers (divisible by 2) between 1 and 10.'
-    pre1.classList.toggle('hidden')
+    h2.innerText = 'Parity';
+    p1.innerText = 'Check the following program that shows even numbers (divisible by 2) between 1 and 10.';
+    pre1.classList.toggle('hidden');
     code.innerHTML = `
     for (let i = 1; i <= 10; i++) {
         if (i % 2 === 0) {
-            console.log('\${i}\ is even');
+            console.log(\`\${i} is even\`);
         }
     }
-  `
-    p2.innerText = 'Improve the program so that it also shows odd numbers. Improve it again to replace the initial number 1 by a number given by the user.'
-    
-    for (let i = 1; i <= 10; i++) {
-        if (i % 2 === 0) {
-            console.log('${i} is even');
+    `;
+    p2.innerText = 'Improve the program so that it also shows odd numbers. Improve it again to replace the initial number 1 by a number given by the user.';
+    d1.innerHTML = `<input id='userParityNum' placeholder='Enter a number'><button id='runParity'>Send it!</button>`;
+
+    const button = document.querySelector('#runParity');
+    const input = document.querySelector('#userParityNum');
+
+    button.addEventListener('click', function () {
+        const userInput = parseInt(input.value);
+
+        if (isNaN(userInput)) {
+            console.error("Please enter a valid number.");
+            alert("Please enter a valid number.");
+            return;
         }
+
+        parity(userInput);
+    });
+
+    function parity(start) {
+        console.log(`Starting parity check from: ${start}`);
+        for (let i = 1; i <= start; i++) {
+            if (i % 2 === 0) {
+                console.log(`${i} is even`);
+            } else {
+                console.log(`${i} is odd`);
+            }
+        }
+        p3.innerHTML = `<i class='bx bx-question-mark' id='nextProblem'></i>`
+
+        document.querySelector('#nextProblem').addEventListener('click', inputValidation);
     }
 }
 
-// Let's try this the prep way. 
-// P
-// R
-// E
-// P
+// Multiplication Table? 
+
+function inputValidation(){
+        removePreviousInfo();
+        pre1.classList.toggle('hidden');
+
+        h2.innerText = 'Number Duel';
+        p1.innerText = 'Guess the random number between 1 and 10. You have 3 lives to get it right or EXPLODE!'
+        d1.innerHTML = `<input id='userGuess'><button id='checkIt'>Check</button>`
+
+
+}
+
 
 
 
