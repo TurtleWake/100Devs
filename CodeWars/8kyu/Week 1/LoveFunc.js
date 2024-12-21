60f84d4698fd9bf0f3e23d63d4177c94de34476d
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2024-12-21 07:11:18 -0500

    Day 4 of CodeWars, I figured this one out on my own, let's go!

---
+++ CodeWars/8kyu/Week 1/LoveFunc.js
@@ -0,0 +1,35 @@
+function lovefunc(flower1, flower2){
+    
+    if(flower1 % 2 === 0 && flower2 % 2 === 1){
+        return true;
+    } else if(flower1 % 2 === 1 && flower2 % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(lovefunc(1,4), true)
+console.log(lovefunc(2,2), false)
+console.log(lovefunc(0,1), true)
+console.log(lovefunc(0,0), false)
+
+/*
+Timmy & Sarah think they are in love, 
+but around where they live, they will only know once they pick a flower each. 
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+***Sample Tests***
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(lovefunc(1,4), true)
+    assert.strictEqual(lovefunc(2,2), false)
+    assert.strictEqual(lovefunc(0,1), true)
+    assert.strictEqual(lovefunc(0,0), false)
+  });
+});
+
+*/
+
